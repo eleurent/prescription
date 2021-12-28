0c5e5fc4f29f055a224d391451ecbdc9429433ff
Edouard Leurent <eleurent@gmail.com>
2021-12-28 17:27:50 +0000

    Graphe termine pour aggression

--- src/component/tree.jsx
+++ src/component/tree.jsx
@@ -2,57 +2,115 @@ import React, { Component } from "react";
 import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 
+function datePlusYears(date, years) {
+	date = new Date(date.getTime());
+	date.setFullYear(date.getFullYear() + years);
+	return date
+}
 
-const dateCheck = (context) => {
-	if (!context.dateOfFacts)
+const dateCheck_1 = (context) => {
+	const date = ("prescriptionDate" in context) ? context.prescriptionDate : context.dateOfFacts;
+	if (!date)
 		return "error";
-	if (context.dateOfFacts < new Date('1989-07-04')) {
-		return "dateCheck_1";
-	} else if (context.dateOfFacts < new Date('1998-06-18')) {
-		return "dateCheck_2";
-	} else if (context.dateOfFacts < new Date('2004-03-10')) {
-		return "dateCheck_3";
-	} else if (context.dateOfFacts < new Date('2017-03-01')) {
-		return "dateCheck_4";
+	if (date < new Date('1989-07-04')) {
+		return "dateCheck_1_1";
+	} else if (date < new Date('1998-06-18')) {
+		return "dateCheck_1_2";
+	} else if (date < new Date('2004-03-10')) {
+		return "dateCheck_1_3";
+	} else if (date < new Date('2017-03-01')) {
+		return "dateCheck_1_4";
 	} else {
-  	return "dateCheck_5";
+  	return "dateCheck_1_5";
   }
 };
 
-const dateCheck_1 = (context) => {
-	context.prescriptionDate = new Date(context.dateOfFacts.getTime());
-	context.prescriptionDate.setFullYear(context.prescriptionDate.getFullYear() + 3);
+const dateCheck_1_1 = (context) => {
+	context.prescriptionDate = datePlusYears(context.dateOfFacts, 3);
 	if (context.prescriptionDate < new Date('1989-07-04'))
 		return context.prescriptionDate;
 	else
-		return "dateCheck_2";
+		return "dateCheck_1";
 };
 
-const dateCheck_2 = (context) => {
+const dateCheck_1_2 = (context) => {
 	if (context.isMinor && context.circAutorite)
-		return "dateCheck_2_1";
+		return "dateCheck_1_2_1";
 	else
-		return "dateCheck_2_2";
+		return "prescriptionSocle";
 };
 
-const dateCheck_2_1 = (context) => {
-	context.prescriptionDate = new Date(context.dateOfBirth.getTime());
-	context.prescriptionDate.setFullYear(context.prescriptionDate.getFullYear() + 21);
-	return context.prescriptionDate;
+const dateCheck_1_2_1 = (context) => {
+	context.prescriptionDate = datePlusYears(context.dateOfBirth, 21);
+	if (context.prescriptionDate > new Date('1998-06-18'))
+		return "dateCheck_1";
+	else
+		return context.prescriptionDate;
 }
 
-const dateCheck_2_2 = (context) => {
-  context.prescriptionDate = new Date(context.dateOfFacts.getTime());
-	context.prescriptionDate.setFullYear(context.prescriptionDate.getFullYear() + 3);
-	return context.prescriptionDate;
+const prescriptionSocle = (context) => {
+  context.prescriptionDate = datePlusYears(context.dateOfFacts, 3);
 }
+
+const prescriptionSocle6 = (context) => {
+  context.prescriptionDate = datePlusYears(context.dateOfFacts, 6);
+}
+
+const dateCheck_1_3 = (context) => {
+	if (context.isMinor) {
+    if (context.isMinor15 && (context.circAutorite || context.circFonctions || context.circCoaction || context.circArme || context.circBlessures)) {
+    	context.prescriptionDate = datePlusYears(context.dateOfBirth, 28);
+		} else {
+			context.prescriptionDate = datePlusYears(context.dateOfBirth, 21);
+		}
+		if (context.prescriptionDate > new Date('2004-03-10'))
+  		return "dateCheck_1";
+  	else
+  		return context.prescriptionDate;
+	}
+	else
+		return "prescriptionSocle";
+};
+
+const dateCheck_1_4 = (context) => {
+	if (context.isMinor) {
+    if (context.isMinor15 && (context.circAutorite || context.circFonctions || context.circCoaction || context.circArme || context.circBlessures)) {
+    	context.prescriptionDate = datePlusYears(context.dateOfBirth, 38);
+		} else {
+			context.prescriptionDate = datePlusYears(context.dateOfBirth, 28);
+		}
+		if (context.prescriptionDate > new Date('2017-03-01'))
+  		return "dateCheck_1";
+  	else
+  		return context.prescriptionDate;
+	}
+	else
+		return "prescriptionSocle";
+};
+
+const dateCheck_1_5 = (context) => {
+	if (context.isMinor) {
+    if (context.isMinor15) {
+    	context.prescriptionDate = datePlusYears(context.dateOfBirth, 38);
+		} else {
+			context.prescriptionDate = datePlusYears(context.dateOfBirth, 28);
+		}
+		return context.prescriptionDate;
+	}
+	else
+		return "prescriptionSocle6";
+};
 
 const nodes = {
-  	dateCheck: {rule: dateCheck},
   	dateCheck_1: {rule: dateCheck_1},
-  	dateCheck_2: {rule: dateCheck_2},
-  	dateCheck_2_1: {rule: dateCheck_2_1},
-  	dateCheck_2_2: {rule: dateCheck_2_2},
+  	dateCheck_1_1: {rule: dateCheck_1_1},
+  	dateCheck_1_2: {rule: dateCheck_1_2},
+  	dateCheck_1_2_1: {rule: dateCheck_1_2_1},
+  	dateCheck_1_3: {rule: dateCheck_1_3},
+  	dateCheck_1_4: {rule: dateCheck_1_4},
+  	dateCheck_1_5: {rule: dateCheck_1_5},
+  	prescriptionSocle: {rule: prescriptionSocle},
+  	prescriptionSocle6: {rule: prescriptionSocle6},
 };
 
 const Node = (props) => {
@@ -65,7 +123,7 @@ const Node = (props) => {
 class Tree extends React.Component {
   constructor(props) {
   	super(props);
-    this.state = {root: "dateCheck"}
+    this.state = {root: "dateCheck_1"}
   }
 
   traverse(inputValues) {
@@ -88,7 +146,7 @@ class Tree extends React.Component {
   	console.log(path);
   	return (<div>
    { path.map((node, idx) => <Node key={idx} value={node}/>) }
-  </div>
+    </div>
   	)
   }
 }
