c46275005021dc1ca0fa24c85ccac8e5c3a6d4e5
Edouard Leurent <eleurent@gmail.com>
2021-12-28 21:32:14 +0000

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Calculateur de Prescription/i);
   expect(linkElement).toBeInTheDocument();
 });
