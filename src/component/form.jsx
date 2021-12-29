c313f61e061e484e61969f39f7b531021a0fb795
Edouard Leurent <eleurent@gmail.com>
2021-12-29 14:03:20 +0000

    Update form

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class InputForm extends React.Component {
   render() {
     return (
       <Form onSubmit={this.props.onSubmit} onChange={this.props.onChange}>
+        <h2>Nature de l'acte</h2>
+        <div class="alert alert-light" role="alert">
+        Il convient dans un premier temps de déterminer la nature de l'acte que vous souhaitez dénoncer :
+        <ul>
+          <li>Constitue une <mark>agression sexuelle</mark> le fait d'imposer à une personne un acte quelconque pouvant porter atteinte à son intégrité sexuelle ou à son intimité. Cette atteinte peut prendre des formes très diverses : attouchements sur les parties génitales, caresses sur le corps, baisers, frottement, déshabillement forcé... La jurisprudence exige que soit rapportée la preuve d'un contact physique, connoté sexuellement.</li>
+          <li>Le <mark>viol</mark> consiste dans tout acte de pénétration sexuelle, quelle qu'en soit la nature. Il peut s'agir de pénétration digitale, pénienne, linguale, ou à l'aide d'un objet, à l'intérieur du vagin, du rectum, ou de la bouche.</li>
+          <li>Le <mark>rapport oro-génital</mark> consiste à mettre en contact le sexe d'une personne, et la bouche d'une autre personne (action de lécher, d'embrasser, etc). Attention, en cas de pénétration, il s'agit d'un viol.</li>
+        </ul>
+        On distinguera le cas où l'acte de pénétration a été commis sur la personne de l'auteur (par exemple : une personne pratique une fellation sur une victime non consentante), ou sur la personne de la victime (cas le plus courant, c'est alors la victime qui est visée par l'acte de pénétration). 
+        </div>
         <Form.Group className="mb-3" controlId="natureActe">
           <Form.Label>Nature de l'acte</Form.Label>
           <Form.Select name="natureActe" aria-label="Nature de l'acte">
@@ -14,6 +25,7 @@ class InputForm extends React.Component {
           </Form.Select>
         </Form.Group>
 
+        <h2>Circonstances</h2>
         <Form.Group className="mb-3" controlId="circAutorite">
           <Form.Check name="circAutorite" type="checkbox" label="Par personne ayant autorité ou ascendance sur la victime" />
         </Form.Group>
@@ -29,15 +41,20 @@ class InputForm extends React.Component {
         <Form.Group className="mb-3" controlId="circBlessures">
           <Form.Check name="circBlessures" type="checkbox" label="Ayant entrainé des blessures" />
         </Form.Group>
+        <h2>Dates</h2>
         <Row className="mb-3">
+          <Col>
           <Form.Group controlId="dateOfBirth">
             <Form.Label>Date de naissance de la victime</Form.Label>
             <Form.Control name="dateOfBirth" type="date" defaultValue={this.props.inputValues.dateOfBirth} onChange={this.props.handleChange}/>
           </Form.Group>
+          </Col>
+          <Col>
           <Form.Group controlId="dateOfFacts">
             <Form.Label>Date des faits</Form.Label>
             <Form.Control name="dateOfFacts" type="date"  defaultValue={this.props.inputValues.dateOfFacts} onChange={this.props.handleChange}/>
           </Form.Group>
+          </Col>
         </Row>
       </Form>
     );
